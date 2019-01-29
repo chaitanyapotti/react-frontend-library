64f818e1c5965d27fcd4d5addedcf6508482135c
aayush2252 <aayushgupta1991@gmail.com>
2019-01-29 18:02:26 +0530

    changes

--- test/src/App.js
+++ test/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-import { CuiButton, CuiLoadingButton } from "react-frontend-library";
+import { CuiButton } from "react-frontend-library";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <CuiButton variant="text" themeEngine="vault">
+        <CuiButton variant="text" themeEngine="semantic">
           Hello
         </CuiButton>
       </div>
